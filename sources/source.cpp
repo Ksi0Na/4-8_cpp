aa8b6b947f1f75616e1d6b12e812d2b49878901a
Ksi0Na <OlgaMolchun20010615@yandex.ru>
2021-03-30 14:52:12 +0300

    done_now

--- sources/source.cpp
+++ sources/source.cpp
@@ -6,4 +6,5 @@ int main(int argc, char* argv[]) {
   Client client(argc, argv);
   return client.Run();
 }
-
+//./cmake-build-debug/tests  http://localhost:8080/v1/api/suggest
+//{\"input\":\"hel\"}
